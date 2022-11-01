da0d84f32a8b831af0e2ad4b675202016f00dd17
twonirwana <jvs@mailbox.org>
2022-11-01 21:42:37 +0100

    better Threadsafe Random Number Supplier

--- src/main/java/de/janno/evaluator/dice/random/RandomNumberSupplier.java
+++ src/main/java/de/janno/evaluator/dice/random/RandomNumberSupplier.java
@@ -7,21 +7,22 @@ import com.google.common.base.Preconditions;
  * Provides random numbers
  */
 public class RandomNumberSupplier implements NumberSupplier {
-    private final Sfc64Random randomNumberGenerator;
+    private final ThreadLocalSfc64Random randomSource;
 
-    public RandomNumberSupplier() {
-        randomNumberGenerator = new Sfc64Random();
+    public RandomNumberSupplier(){
+        randomSource = new ThreadLocalSfc64Random();
     }
 
     @VisibleForTesting
     RandomNumberSupplier(long seed) {
-        randomNumberGenerator = new Sfc64Random(seed);
+        randomSource = new ThreadLocalSfc64Random(seed);
     }
 
-    @Override
     public int get(int minExcl, int maxIncl) {
         Preconditions.checkArgument(minExcl < maxIncl, "minExcl must be smaller than maxIncl");
-        int diff = maxIncl - minExcl + 1;
-        return (int) (minExcl + (randomNumberGenerator.nextDouble() * diff));
+        if (minExcl + 1 == maxIncl) {
+            return maxIncl;
+        }
+        return randomSource.nextInt(minExcl, maxIncl + 1);
     }
 }
--- src/test/java/de/janno/evaluator/dice/random/RandomNumberSupplierTest.java
+++ src/test/java/de/janno/evaluator/dice/random/RandomNumberSupplierTest.java
@@ -14,25 +14,25 @@ class RandomNumberSupplierTest {
 
     @Test
     void d6Test() {
-        List<Integer> res = IntStream.range(0, 10_000)
+        List<Integer> res = IntStream.range(0, 1_000_000)
                 .map(i -> underTest.get(1, 6))
                 .boxed()
                 .toList();
 
         assertThat(res).allMatch(i -> i >= 1 && i <= 6);
         assertThat(res).contains(1, 2, 3, 4, 5, 6);
-        assertThat(res.stream().mapToInt(i -> i).average().orElseThrow()).isEqualTo(3.5, offset(0.1));
+        assertThat(res.stream().mapToInt(i -> i).average().orElseThrow()).isEqualTo(3.5, offset(0.01));
     }
 
     @Test
     void from10to100Test() {
-        List<Integer> res = IntStream.range(0, 10_000)
+        List<Integer> res = IntStream.range(0, 1_000_000)
                 .map(i -> underTest.get(10, 100))
                 .boxed()
                 .toList();
 
         assertThat(res).allMatch(i -> i >= 10 && i <= 100);
         assertThat(res).contains(10, 100);
-        assertThat(res.stream().mapToInt(i -> i).average().orElseThrow()).isEqualTo(55, offset(0.1));
+        assertThat(res.stream().mapToInt(i -> i).average().orElseThrow()).isEqualTo(55, offset(0.01));
     }
 }
