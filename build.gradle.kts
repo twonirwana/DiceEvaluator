c6abf24a5051890a30d4eec528448b0cbcadc9db
twonirwana <jvs@mailbox.org>
2023-09-18 12:06:04 +0200

    Java20 (#59)
    * java 20
    
    * update gradle 8.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(18))
+        languageVersion.set(JavaLanguageVersion.of(20))
     }
     withJavadocJar()
     withSourcesJar()
