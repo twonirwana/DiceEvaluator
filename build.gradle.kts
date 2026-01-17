5455cba0aa4132d575d5d757c10f123cbefa028f
twonirwana <jvs@mailbox.org>
2026-01-17 11:15:46 +0100

    java 25 fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,8 +31,9 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_25
-    targetCompatibility = JavaVersion.VERSION_25
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
     withJavadocJar()
     withSourcesJar()
 }
